eed60f833accb10a1b027c9f48268bc38e549dec
vikash_sahu <vikash13061992@gmail.com>
2021-10-21 14:20:33 +0530

    Test : OrderProcessingEngine Unit Test

---
+++ OrderProcessingEngine.UnitTest/Controller/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using OrderProcessingEngine.Controllers;
+using Xunit;
+
+namespace OrderProcessingEngine.UnitTest.Controller
+{
+    [Collection("HomeControllerTest")]
+    public class HomeControllerTest
+    {
+
+        [Fact]
+        public void Calling_Index_Returns_View()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            var result = controller.Index();
+            // Assert
+            Assert.NotNull(result);
+        }
+
+    }
+}

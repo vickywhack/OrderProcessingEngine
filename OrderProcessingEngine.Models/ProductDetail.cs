a8e713235f27a4b24796f8d76a416a433c03029b
vikash_sahu <vikash13061992@gmail.com>
2021-10-21 12:57:46 +0530

    App : Models Created

---
+++ OrderProcessingEngine.Models/ProductDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OrderProcessingEngine.Models
+{
+    public class ProductDetail
+    {
+        public int ProductId { get; set; }
+        public ProductType ProductType { get; set; }
+        public string ProductName { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        UnProcessed,
+        Processed
+    }
+
+    public enum ProductType
+    {
+        PhysicalProduct,
+        Book,
+        NewMemberShip,
+        UpgradeMembership,
+        Video,
+    }
+}
